bc862f12b37f7acc802081cd179b1248c9fc45bc
A-star-ayush <myselfthebest@yahoo.com>
2018-10-21 00:27:49 -0400

    deleted the unoptimized palindrome solution

--- Recursion/palindrome.c
+++ Recursion/palindrome.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int isPalindrome(char* str){
-	const int len=strlen(str);
-	if(len<=1)							// Base case: empty string and string of length 1 are both palindromic
+// Yet another way to check for palindrome is to reverse the string and then compare the original
+// and the reversed string using strcmp
+
+int isPalindrome(char* str, int n){
+	if(n < 1)                      // a single character alone on itself is palindromic
 		return 1;
-	
 	char first=str[0];
-	                                          // for a more efficient implementation see palindrome2.c
-	char sub[len-1];
-	int i;
-	for(i=1;i<len-1;i++)              // creating a substring that excludes the first and the last character
-		sub[i-1]=str[i];
-	sub[i-1]='\0';						// Note the addition of \0 at the last index to terminate the string
-	int mid=isPalindrome(sub);					// a necessary addition without which the program causes errors
-	
-	char last=str[len-1];
-													// Inductive definition:
+	int mid=isPalindrome(str+1,n-2);   // note the use pointer arithmetic and "n-2" and not 1
+	char last=str[n];                        // the reason for -2 is that str has also been increemented by 1
+
 	return (first==last) && mid;   // a string f s l is palindromic is f==l and s is a palindrome itself
 }
 
@@ -26,7 +20,7 @@ int main(int argc, char const *argv[])
 	printf("String (max 10 characters): ");
 	scanf("%10s",str);
 
-	if(isPalindrome(str))
+	if(isPalindrome(str,strlen(str)-1))
 		printf("It is a palindrome.\n");
 	else
 		printf("It is not a palindrome.\n");
